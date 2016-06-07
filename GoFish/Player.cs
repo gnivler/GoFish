09d1923ceac62bad9f922d388d91fa5fa6fd5f0b
gnivler <gnivil@gmail.com>
2016-06-07 17:02:27 -0600

    still copying code

---
+++ GoFish/Player.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GoFish
+{
+    class Player
+    {
+        private string name;
+        public string Name { get { return name; } }
+        private Random random;
+        private Deck cards;
+        private TextBox textBoxOnForm;
+
+        public Player()
+        {
+
+        }
+
+        public IEnumerable<Values> PullOutBooks()
+        {
+
+        }
+
+        public Values GetRandomValue()
+        {
+
+        }
+
+        public Deck DoYouHaveAny(Values value)
+        {
+
+        }
+        
+        public void AskForACard(List<Player> players, int myIndex, Deck stock)
+        {
+
+        }
+
+        public void AskForACard(List<Player> players, int myIndex, Deck stock, Values value)
+        {
+
+        }
+
+        public int CardCount { get { return cards.Count; } }
+        public void TakeCard(Card card) { cards.Add(card); }
+        public IEnumerable<string> GetCardNames() { return cards.GetCardNames(); }
+        public Card Peek(int cardNumber) { return cards.Peek(cardNumber); }
+        public void SortHand() { cards.SortByValue(); }
+    }
+}
